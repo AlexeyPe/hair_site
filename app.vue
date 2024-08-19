cb398c08267117c80c322182529bfe1546f4547a
Alexey Pechenckin <pechenckinalexey@gmail.com>
2024-08-19 22:11:27 +0500

    Установка nuxt/ui, настройка seo

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+<template>
+  <h1>Алексей Пе - окрашивание тотал блонд</h1>
+  <p>Параграф</p>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      htmlAttrs: { lang: 'en' }
+    }
+  },
+  // nitro: {
+  //     compressPublicAssets: true,
+  //     minify: true
+  // },
+})
