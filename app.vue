8a452b11e8f269bd1b6e350382178f53bca0b4b2
Alexey Pechenckin <pechenckinalexey@gmail.com>
2024-08-24 14:48:13 +0500

    Исправление ошибки iframe и редактирование текста цен

--- app.vue
+++ app.vue
@@ -32,10 +32,10 @@ useSeoMeta({
         </CBox>
         <CBox class="h-full w-full">
           <div class="grid grid-cols-2 gap-3" style="padding: 6dvh 0px !important;">  
-            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" height="220px"/>
-            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" height="220px"/>
-            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" height="220px"/>
-            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" height="220px"/>
+            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" width="220px" height="220px"/>
+            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" width="220px" height="220px"/>
+            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" width="220px" height="220px"/>
+            <NuxtImg src="img/natural blonde.webp" alt="блонд натуральный" width="220px" height="220px"/>
           </div>
         </CBox>
       </HBox>
@@ -48,8 +48,8 @@ useSeoMeta({
         <p class="text-lg ">ㅤㅤДлина до подбородка — хххх руб.</p>
         <p class="text-lg ">ㅤㅤДлина около плеч — хххх руб.</p>
         <p class="text-lg ">ㅤㅤДлина около лопаток — хххх руб.</p>
-        <p class="mt-4">*В стоимость входит стрижка кончиков(по пожеланию, для длинных), сушка и простая укладка, уходовые средства во время мытья головы и укладки.</p>
-        <p class="mb-4">**Цены для всех оттенков одинаковы.</p>
+        <p class="mt-4">— В стоимость входит стрижка кончиков(по пожеланию, для длинных), сушка и простая укладка, уходовые средства во время мытья головы и укладки.</p>
+        <p class="mb-4">— Цены одинаковы для всех оттенков(Тотал блонд / Стальной / т.д.).</p>
         <VBox class="mt-12 ">
           <HBox class="mb-1">
             <ButtonTG/>
@@ -70,7 +70,7 @@ useSeoMeta({
             icon="i-heroicons-clipboard-document"
             class="mb-2 w-auto"
           />
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3003.581800566313!2d60.60902666858604!3d56.81025832309467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16fa82b188ae7%3A0xc3871d48ad6b839c!2z0JHRjNGO0YLQuC3QmtC-0LLQvtGA0LrQuNC90LMgQ2hlcnJ5!5e1!3m2!1sru!2sru!4v1724447634591!5m2!1sru!2sru" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe title="карта расположения салона" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3003.581800566313!2d60.60902666858604!3d56.81025832309467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c16fa82b188ae7%3A0xc3871d48ad6b839c!2z0JHRjNGO0YLQuC3QmtC-0LLQvtGA0LrQuNC90LMgQ2hlcnJ5!5e1!3m2!1sru!2sru!4v1724447634591!5m2!1sru!2sru" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </VBox>
       </VBox>
     </div>
