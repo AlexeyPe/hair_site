22df0bd397d84f841eaf90532c331a5d2ccced67
Alexey Pechenckin <pechenckinalexey@gmail.com>
2024-08-30 01:27:05 +0500

    Фикс контраста хедера

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="fixed top-0 left-0 right-0">
+  <header class="fixed top-0 left-0 right-0 dark:bg-[#1212125e]">
     <UContainer class="flex items-center backdrop-blur max-md:flex-col">
       <h1 class="tracking-tight font-normal text-base md:w-1/3 max-md:mt-1.5">Алексей Пе | окрашивание блонд</h1>
       <HBox class="w-2/3">
